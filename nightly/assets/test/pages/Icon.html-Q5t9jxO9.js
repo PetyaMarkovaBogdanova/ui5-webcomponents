8510caad08e105959073ac57970b4efa4456ea00
PetyaMarkovaBogdanova <PetyaMarkovaBogdanova@users.noreply.github.com>
2024-11-27 00:44:28 +0000

    Deploying to gh-pages from @ PetyaMarkovaBogdanova/ui5-webcomponents@3bcfd1d26b8520b73be336558f590e1b89c4d0c3 🚀

---
+++ nightly/assets/test/pages/Icon.html-Q5t9jxO9.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-DYwVqAG-.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
