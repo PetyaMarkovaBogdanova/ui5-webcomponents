8c2a9f60c4c65d079978aabda6229a3f59d9d508
PetyaMarkovaBogdanova <PetyaMarkovaBogdanova@users.noreply.github.com>
2025-01-07 08:26:09 +0000

    Deploying to gh-pages from @ PetyaMarkovaBogdanova/ui5-webcomponents@5a0ad53245d1587f9c7efaeb796fbca44aac8d56 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1734446238,
+    buildTime: 1736238138,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
